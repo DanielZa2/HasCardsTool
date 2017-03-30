19916c1c4f56014c85514be4fb77996c6ee2c962
Daniel Zatulovsky <DanielZa2u2@gmail.com>
2017-03-30 19:01:37 +0300

    Whoops. Screwed up the merge. Fixed.

--- Main.py
+++ Main.py
@@ -2,10 +2,7 @@ import json
 import os
 import datetime
 import collections
-<<<<<<< HEAD
-=======
 import time
->>>>>>> Early_Backend
 import logging
 
 from urllib import request as urlrequest
@@ -20,44 +17,15 @@ class ParseException(Exception):
 
 
 
-<<<<<<< HEAD
-def simplified_name(name):
-    ret = name.strip()
-    ret = ret.translate(None, "™®©!,.'[](){}")  # Remove these characters from the string
-    for ch in ["_", "-", ":", ";"]:
-        if ch in ret:
-            ret = ret.replace(ch, " ")
-
-    # ret = ret.replace("'s", "")
-    ret = ret.replace("&", " and ")
-    return ret
-
-
-def fetch_game_list(path):
-    with open(path, encoding='UTF-8') as file:
-        names = file.read().splitlines()
-        return map(SteamApp, names)
-
-
-
-
-=======
->>>>>>> Early_Backend
 
 class SteamApp:
     """Describe a single steam app. That is, more often than not, a game."""
 
     def __init__(self, name):
         self.id = None
-<<<<<<< HEAD
-        self.simplified_name = simplified_name(name)
-        self.users_name = name
-        self.known_cards = False
-=======
         self.users_name = name
         self.simplified_name = simplified_name(name)
         self.card_status_known = False
->>>>>>> Early_Backend
         self.has_cards = False
 
     def __str__(self):
@@ -66,13 +34,6 @@ class SteamApp:
     def __repr__(self):
         return "<SteamApp: " + self.users_name + ">"
 
-<<<<<<< HEAD
-    def get_id(self, applist):
-        """Lookup your own name in the supplied list of names."""
-        self.id = applist.name_lookup.get(self.simplified_name, None)  # default=None
-        return self.id
-
-=======
     def find_id(self, applist):
         """Lookup your own id in the supplied list."""
         self.id = applist.name_lookup.get(self.simplified_name, None)  # default=None
@@ -121,7 +82,6 @@ class SteamApp:
                 self.has_cards = True
                 break
 
->>>>>>> Early_Backend
 
 class SteamAppList:
     """Describe a list of appIDs and app names. Used to find the name of the app based on the id."""
@@ -168,14 +128,6 @@ class SteamAppList:
             logging.exception("Failed to parse fetched applist")
             return None
 
-<<<<<<< HEAD
-    def get_id(self, lst):
-        """Give names to all the apps in the list,"""
-        for game in lst:
-            game.get_id(self)
-
-=======
->>>>>>> Early_Backend
     def fetch(self, fetch_from_net=False):
         """Fill the object with data about app names. get the data either from a local file or from the internet. Automaticlly access the net if the file is missing."""
         if self.__data__ is not None:
@@ -189,35 +141,6 @@ class SteamAppList:
             self.__data__ = SteamAppList.json_to_list(SteamAppList.fetch_from_disk())
 
         self.id_lookup = {pair["appid"]: pair["name"] for pair in self.__data__}
-<<<<<<< HEAD
-        self.name_lookup = {simplified_name(pair["name"]): pair["appid"] for pair in self.__data__}
-        return self
-
-
-@staticmethod
-def fetch_game_data_static(app_id):
-    req = urlrequest.Request("http://store.steampowered.com/api/appdetails/?appids=" + app_id)
-
-    try:
-        json_bytes = urlrequest.urlopen(req).read()
-    except urlerror.HTTPError:
-        logging.exception("Failed getting details for app " + app_id)
-        return None
-
-    json_text = json_bytes.decode("utf-8")
-    try:
-        game_info = json.loads(json_text)
-
-        if not game_info[app_id]["success"]:
-            return None
-
-        data = game_info[app_id]["data"]
-        return data
-
-    except (json.decoder.JSONDecodeError, KeyError):
-        logging.exception("Failed to parse details for app " + app_id)
-        return None
-=======
         self.name_lookup = {simplified_name(pair["name"]): str(pair["appid"]) for pair in self.__data__}
         return self
 
@@ -291,28 +214,13 @@ def export_csv(games, filename="output.csv"):
             file.write(line)
             file.write("\n")
             print(line)
->>>>>>> Early_Backend
 
 
 def main():
     path = "Test/list.txt"
-<<<<<<< HEAD
-    applist = SteamAppList().fetch()
-    input_games = fetch_game_list(path)
-    applist.get_id(input_games)
-
-
-
-
-
-
-
-
-=======
     input_games = fetch_users_game_list(path)
     fetch_card_info(input_games)
     export_csv(input_games, "Test/list_out.csv")
->>>>>>> Early_Backend
 
 
 if __name__ == "__main__":
